900d51b194224c0a095330975e63e247d6e5efd8
vagrant <vagrant@localhost.localdomain>
2020-07-22 05:56:43 +0000

    [Add]ログイン機能生徒用/先生用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	# ログアウト後に遷移するpathを設定
+	def after_sign_out_path_for(resource)
+    	new_student_session_path
+	end
+
+	protected
+	# サインアップ時、devise初期設定に追加したカラムを記載
+	def configure_permitted_parameters
+  		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :sub_last_name, :sub_first_name])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'teachers/index'
+  root 'students#show'
+  devise_for :teachers, controllers: {
+  	sessions: 'teachers/sessions',
+  	passwords: 'teachers/passwords',
+  	registrations: 'teachers/registrations'
+  }
+
+  devise_for :students, controllers: {
+  	sessions: 'students/sessions',
+  	passwords: 'students/passwords',
+  	registrations: 'students/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
