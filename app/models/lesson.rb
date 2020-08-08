e48467bcd9626335836f83bcb272bd1de8d381a3
vagrant <vagrant@localhost.localdomain>
2020-08-08 20:04:19 +0000

    [Fix]バリデーション 日本語化

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,12 +1,14 @@
 class Lesson < ApplicationRecord
-  belongs_to :genre
-  belongs_to :area_class
-  belongs_to :teacher
-  belongs_to :time_class
+  # 外部キーは空白だとNG、データは絶対必要。でも↓のオプションはデータなくてもいいよ、というおまじない。
+  belongs_to :area_class, optional: true
+  belongs_to :teacher, optional: true
+  belongs_to :genre, optional: true
+  belongs_to :time_class, optional: true
   has_many :attendees
   has_many :no_lessons
   attachment :image
   validates :area_class_id, presence: true
   validates :genre_id, presence: true
+  validates :time_class_id, presence: true
   validates :teacher_id, presence: true
 end
