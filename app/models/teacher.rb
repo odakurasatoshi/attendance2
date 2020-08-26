164016c282015f1d86dc66f3d13a9e2e0ae97932
vagrant <vagrant@localhost.localdomain>
2020-08-26 05:33:45 +0000

    [Add]ログイン時のエラーメッセージ

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -9,7 +9,7 @@ class Teacher < ApplicationRecord
   validates :last_name, presence: true
   validates :sub_last_name, presence: true
   validates :first_name, presence: true
-  validates :sub_last_name, presence: true
+  validates :sub_first_name, presence: true
   validates :email, presence: true
 
   def self.guest
