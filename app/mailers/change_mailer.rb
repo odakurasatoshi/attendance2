67e1c11084116a5a45e977256ac1a351df24f950
vagrant <vagrant@localhost.localdomain>
2020-09-27 13:09:26 +0000

    [Change]メール送信先アドレス

--- app/mailers/change_mailer.rb
+++ app/mailers/change_mailer.rb
@@ -2,6 +2,6 @@ class ChangeMailer < ApplicationMailer
   def send_change(current_student, course_id)
     @student = current_student
     @course = course_id
-    mail to: 'guerreiro88923@gmail.com', subject: 'コース変更依頼'
+    mail to: 'accc.chiba.select@gmail.com', subject: 'コース変更依頼'
   end
 end
